e000110e682d18f9bef4520da1f711cd37507e6e
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-01-19 04:45:07 +0900

    Create 1000.cpp

---
+++ BOJ/1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+    cout << a+b;
+    
+    return 0;
+}
