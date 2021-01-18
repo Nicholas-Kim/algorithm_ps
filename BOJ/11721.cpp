4cfe7e8cc10d3a528e24044a70a7d93cb4881372
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-01-19 05:47:29 +0900

    Create 11721.cpp

---
+++ BOJ/11721.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char a[100];
+    while(scanf("%10s", a)==1){
+        printf("%s\n", a);
+    }
+    return 0;
+}
