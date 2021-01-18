8b6d94b966ad84f78a4315b7fdb842d268ecbfed
HyunSoo Kim <39149858+Nicholas-Kim@users.noreply.github.com>
2021-01-19 05:38:41 +0900

    Create 11719.cpp

---
+++ BOJ/11719.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    char c;
+    while ((c = getchar()) && c != EOF) {
+        printf("%c",c);
+    }
+    
+    return 0;
+}
